254f7140b945f58784a6fec7099b021bbd6899c8
GhosTHaise <Ghostrex2@gmail.com>
2023-06-09 19:17:39 +0300

    Get upload url

--- app/controllers/BackblazeController.js
+++ app/controllers/BackblazeController.js
@@ -9,16 +9,28 @@ const updateExistingKey = async (req,res) => {
     try {
         //
         const authorization_key_headers = Buffer.from(`${process.env.keyID}:${process.env.applicationKey}`).toString('base64');
-        const request = await fetch("https://api.backblazeb2.com/b2api/v2/b2_authorize_account",{
+        const request_authorization_token = await fetch("https://api.backblazeb2.com/b2api/v2/b2_authorize_account",{
             method : "GET",
             headers : {
                 "Authorization" : `Basic ${authorization_key_headers}`
             },
         });
-        const {authorizationToken} = await request.json(); 
+        const {authorizationToken} = await request_authorization_token.json(); 
         //We have GOT TOKEN
+        const request_uploadUrl  = await fetch("https://api005.backblazeb2.com/b2api/v2/b2_get_upload_url",{
+            method : "GET",
+            headers : {
+                "Authorization" : authorizationToken
+            },
+            body : {
+                bucketId : process.env.BUCKET_ID
+            }
+        })
+        
+        const upload_url = await request_uploadUrl.json();
+        
         res.status(200).json({
-            key : authorizationToken
+            key : upload_url
         })
     } catch (error) {
         console.log(error);
@@ -28,3 +40,5 @@ const updateExistingKey = async (req,res) => {
 export {
     updateExistingKey
 }
+
+//curl -H 'Authorization: 4_005f1b3c53e8cde0000000001_01ace089_5f1bd2_acct_IcIhm5CXKp2u2a4Z8-TDmvqawc4=' -d '{"bucketId": "3ff1db23dc15439e888c0d1e"}' https://api005.backblazeb2.com/b2api/v2/b2_get_upload_url
